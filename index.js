7acce0fe50fe1c1c01824f0688c9ff816f6a145a
TomDev0 <77364814+TomDev0@users.noreply.github.com>
2021-01-12 18:18:24 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+const { Client, Collection } = require("discord.js");
+const { config } = require("dotenv");
+const { token, default_prefix } = require("./config.json")
+const client = new Client({
+    disableEveryone: true
+})
+
+
+client.commands = new Collection();
+client.aliases = new Collection();
+
+
+
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+client.on("ready", () => {
+    console.log(`oye ${client.user.username} esta online!`);
+
+    client.user.setPresence("EN beta") 
+})
+
+client.on("message", async message => {
+   
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(default_prefix)) return;
+
+    
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(default_prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+ 
+    let command = client.commands.get(cmd);
+
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+  
+    if (command) 
+        command.run(client, message, args);
+});
+
+client.login(token);
